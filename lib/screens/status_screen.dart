30123a803a1da17609128757d227b683f500f8f9
ujjwal-here <ujjwalsbx@gmail.com>
2021-02-14 23:47:30 +0530

    status icons added

--- lib/screens/status_screen.dart
+++ lib/screens/status_screen.dart
@@ -49,19 +49,96 @@ class _StatusScreenState extends State<StatusScreen> {
             ),
             child: Column(
               children: [
-                Center(
-                  child: Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(1000),
-                        border: Border.all(color: Colors.indigo, width: 3)),
-                    child: const CircleAvatar(
-                      radius: 70,
-                      backgroundImage: const AssetImage(
-                          "assets/images/pexels-sindre-strøm-1040880.jpg"),
-                    ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(1000),
+                      border: Border.all(
+                          color: Color.fromRGBO(231, 233, 240, 1), width: 3)),
+                  child: const CircleAvatar(
+                    radius: 50,
+                    backgroundImage: const AssetImage(
+                        "assets/images/pexels-sindre-strøm-1040880.jpg"),
                   ),
                 ),
+                Container(
+                  margin: EdgeInsets.only(top: 10),
+                  child: Text(
+                    "Your Status",
+                    style: GoogleFonts.nunito(
+                        fontWeight: FontWeight.w900,
+                        fontSize: 20,
+                        color: Colors.black87),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 40, right: 40, top: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(1000),
+                              gradient: LinearGradient(
+                                colors: [
+                                  Color.fromRGBO(67, 127, 199, 1),
+                                  Color.fromRGBO(109, 175, 254, 1)
+                                ],
+                              ),
+                            ),
+                            child: Icon(
+                              Icons.create_rounded,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 10),
+                            child: Text(
+                              " Text Status",
+                              style: GoogleFonts.nunito(
+                                  fontWeight: FontWeight.w800,
+                                  fontSize: 15,
+                                  color: Colors.black87),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(1000),
+                              gradient: LinearGradient(
+                                colors: [
+                                  Color.fromRGBO(67, 127, 199, 1),
+                                  Color.fromRGBO(109, 175, 254, 1)
+                                ],
+                              ),
+                            ),
+                            child: Icon(
+                              Icons.camera_alt_rounded,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 10),
+                            child: Text(
+                              "Media Status",
+                              style: GoogleFonts.nunito(
+                                  fontWeight: FontWeight.w800,
+                                  fontSize: 15,
+                                  color: Colors.black87),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
               ],
             ),
           ),
